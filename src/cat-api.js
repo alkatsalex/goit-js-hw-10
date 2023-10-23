8323f7bc42a85c52cf21a8b76d8a23f6e8597bd5
alkatsalex <alkatsalex@ukr.net>
2023-10-23 16:56:30 +0300

    try2 task done

---
+++ src/cat-api.js
@@ -0,0 +1,65 @@
+import axios from "axios";
+import Notiflix from 'notiflix';
+
+
+axios.defaults.headers.common["x-api-key"] = "live_heLL8lPfd4A2Lpv2S81Bs4OmGBDqCQu3c6GUdGoDG7DG6OuoRNBt0u87Pw8zAWuP";
+
+const KeY = "live_heLL8lPfd4A2Lpv2S81Bs4OmGBDqCQu3c6GUdGoDG7DG6OuoRNBt0u87Pw8zAWuP"
+
+
+
+export const el = {
+    catCard:document.querySelector('.cat-info'),
+    breedSelect: document.querySelector('.breed-select'),
+    error: document.querySelector('.error'),
+    loader: document.querySelector('.loader')
+}
+
+console.log(el.loader)
+
+export function fetchBreeds() {
+    el.loader.classList.toggle('js-hiden');
+  return  fetch(`https://api.thecatapi.com/v1/breeds?api_key=${KeY}`)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  })
+.then(data => {
+    console.log(data); return data
+}).catch(error => {
+    el.error.classList.toggle('js-hiden');
+    Notiflix.Notify.failure(`😿cat not found`, {timeout: 10000, position: 'center-center'})
+  });
+} 
+
+export function fetchCatByBreed(breedId) {
+    loading()
+    el.catCard.innerHTML = ""
+    return fetch(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}&api_key=${KeY}`)
+    .then(response => {
+        if (!response.ok) {
+          throw new Error(response.status);
+        }
+        return response.json();
+      })
+    .then(
+        data => {
+            console.log(data[0])
+            return data[0]
+        }
+).catch(error => {
+    el.breedSelect.classList.toggle('js-hiden');
+    el.loader.classList.toggle('js-hiden');
+    Notiflix.Notify.failure(`😿cat not found`, {timeout: 10000, position: 'center-center'})
+  });
+}
+
+
+ export function loading() {
+    el.loader.classList.toggle('js-hiden');
+        el.breedSelect.classList.toggle('js-hiden');
+  }
+
+
---
+++ src/index.js
@@ -0,0 +1,66 @@
+import { fetchBreeds, fetchCatByBreed, el, loading } from './cat-api.js';
+import axios from "axios";
+
+
+// axios.defaults.headers.common["x-api-key"] = "live_heLL8lPfd4A2Lpv2S81Bs4OmGBDqCQu3c6GUdGoDG7DG6OuoRNBt0u87Pw8zAWuP";
+const KeY = "live_heLL8lPfd4A2Lpv2S81Bs4OmGBDqCQu3c6GUdGoDG7DG6OuoRNBt0u87Pw8zAWuP"
+
+
+
+
+// // axios.get('https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng')
+// //   .then(function (response) {
+// //     // обработка успешного запроса
+// //     console.log(response);
+// //   })
+// //   .catch(function (error) {
+// //     // обработка ошибки
+// //     console.log(error);
+// //   })
+
+
+
+
+fetchBreeds().then(arr => {
+  loading()
+return arr.map(({name, id}) => 
+`<option value="${id}">${name}</option>`
+).join("")}
+)
+.then(
+markup => {return el.breedSelect.innerHTML = markup;}
+)
+
+el.breedSelect.addEventListener('change', () => {
+  
+  const selectedValue = el.breedSelect.value;
+  fetchCatByBreed(selectedValue)
+  .then( 
+    ({ breeds, url }) => {
+      console.log(breeds);
+        return `<img src="${url}" alt="cat" width = "300px">
+        <h2>${breeds[0].name}</h2>
+        <h3>${breeds[0].temperament}</h3>
+        <p>${breeds[0].description}</p>`
+      }
+  ).then(
+    markup => {
+      loading()
+      return el.catCard.innerHTML = markup
+    }
+  )
+})
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
